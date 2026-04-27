ea966c88586e698e9c2ccf8c76c4cbda43950d32
Глушков Никита <nikitkazoloto052007@gmail.com>
2026-04-27 17:34:36 +0300

    style: rename app title to My Calculator

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "calculator.h"
 
 int main() {
-    std::cout << "=== Simple Calculator ===" << std::endl;
+    std::cout << "=== My Calculator ===" << std::endl;
 
     double a = 10.0;
     double b = 3.0;
