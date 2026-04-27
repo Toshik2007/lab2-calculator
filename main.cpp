98981f0764260644181f09b0869950c964269efe
Глушков Никита <nikitkazoloto052007@gmail.com>
2026-04-27 17:06:35 +0300

    Initial commit: add calculator project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "calculator.h"
+
+int main() {
+    std::cout << "=== Simple Calculator ===" << std::endl;
+
+    double a = 10.0;
+    double b = 3.0;
+
+    std::cout << a << " + " << b << " = " << add(a, b) << std::endl;
+    std::cout << a << " - " << b << " = " << subtract(a, b) << std::endl;
+    std::cout << a << " * " << b << " = " << multiply(a, b) << std::endl;
+    std::cout << a << " / " << b << " = " << divide(a, b) << std::endl;
+
+    return 0;
+}
